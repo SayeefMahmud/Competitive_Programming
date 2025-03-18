bf508b53d19f01f29df0fe2feea5fe67d1a30ee7
Sayeef Mahmud <92873673+SayeefMahmud@users.noreply.github.com>
2025-03-18 12:17:54 +0600

    Create SOS DP Basic.cpp

---
+++ templates/SOS DP Basic.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define fr(m) for(int i=0; i<(m); i++)
+#define fro(m) for(int i=1; i<(m); i++)
+#define frj(m) for(int j=0; j<(m); j++)
+#define frr(n) for(int i=(n); i>=0; i--)
+#define pb push_back
+#define pf push_front
+#define orr ||
+#define nl '\n'
+#define nll cout<<'\n'
+#define mod 1000000007
+#define inf (1LL<<60)
+#define inff (1<<30)-1
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define ff first
+#define ss second
+#define all(v) v.begin(), v.end()
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define mxn changeit
+
+int n, A[(1<<20)+5], dp[22][(1<<20)+5];
+
+int main()
+{
+    fastio;
+    cin>>n;
+    for(int i=0; i<(1<<n); i++){
+        cin>>A[i];
+    }
+    //base case
+    for(int i=0; i<(1<<n); i++){
+        dp[0][i]=A[i];
+    }
+    for(int p=1; p<=n; p++){
+        for(int i=0; i<(1<<n); i++){
+            if(i&(1<<(p-1))){
+                dp[p][i]=dp[p-1][i]+dp[p-1][i^(1<<(p-1))];
+            }
+            else{
+                dp[p][i]=dp[p-1][i];
+            }
+        }
+    }
+
+    //f[i]=dp[n][i]
+    for(int i=0; i<(1<<n); i++){
+        cout<<dp[n][i]<<" ";
+    }
+
+    cout<<"\n";
+
+    return 0;
+}
